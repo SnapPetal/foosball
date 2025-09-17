6791fdd322c4fe1ae768dc5e8412adb13d546de5
thonbecker <thon.becker@gmail.com>
2025-09-17 05:27:23 -0500

    chore: code clean up

--- src/main/java/com/thonbecker/foosball/controller/FoosballController.java
+++ src/main/java/com/thonbecker/foosball/controller/FoosballController.java
@@ -2,6 +2,8 @@ package com.thonbecker.foosball.controller;
 
 import com.thonbecker.foosball.entity.Game;
 import com.thonbecker.foosball.entity.Player;
+import com.thonbecker.foosball.model.CreatePlayerRequest;
+import com.thonbecker.foosball.model.GameRequest;
 import com.thonbecker.foosball.projection.PlayerStats;
 import com.thonbecker.foosball.projection.TeamStats;
 import com.thonbecker.foosball.service.FoosballService;
@@ -161,93 +163,5 @@ public class FoosballController {
                 Map.entry("highestTotalScore", foosballService.getHighestTotalScore()),
                 Map.entry("lowestTotalScore", foosballService.getLowestTotalScore()));
         return ResponseEntity.ok(stats);
-    }
-
-    // Request DTOs
-    public static class CreatePlayerRequest {
-        private String name;
-        private String email;
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
-    }
-
-    public static class GameRequest {
-        private String whiteTeamPlayer1;
-        private String whiteTeamPlayer2;
-        private String blackTeamPlayer1;
-        private String blackTeamPlayer2;
-        private int whiteTeamScore;
-        private int blackTeamScore;
-        private String notes;
-
-        public String getWhiteTeamPlayer1() {
-            return whiteTeamPlayer1;
-        }
-
-        public void setWhiteTeamPlayer1(String whiteTeamPlayer1) {
-            this.whiteTeamPlayer1 = whiteTeamPlayer1;
-        }
-
-        public String getWhiteTeamPlayer2() {
-            return whiteTeamPlayer2;
-        }
-
-        public void setWhiteTeamPlayer2(String whiteTeamPlayer2) {
-            this.whiteTeamPlayer2 = whiteTeamPlayer2;
-        }
-
-        public String getBlackTeamPlayer1() {
-            return blackTeamPlayer1;
-        }
-
-        public void setBlackTeamPlayer1(String blackTeamPlayer1) {
-            this.blackTeamPlayer1 = blackTeamPlayer1;
-        }
-
-        public String getBlackTeamPlayer2() {
-            return blackTeamPlayer2;
-        }
-
-        public void setBlackTeamPlayer2(String blackTeamPlayer2) {
-            this.blackTeamPlayer2 = blackTeamPlayer2;
-        }
-
-        public int getWhiteTeamScore() {
-            return whiteTeamScore;
-        }
-
-        public void setWhiteTeamScore(int whiteTeamScore) {
-            this.whiteTeamScore = whiteTeamScore;
-        }
-
-        public int getBlackTeamScore() {
-            return blackTeamScore;
-        }
-
-        public void setBlackTeamScore(int blackTeamScore) {
-            this.blackTeamScore = blackTeamScore;
-        }
-
-        public String getNotes() {
-            return notes;
-        }
-
-        public void setNotes(String notes) {
-            this.notes = notes;
-        }
     }
 }
