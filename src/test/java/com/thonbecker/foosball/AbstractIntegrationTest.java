944ae1678325f54f01bec2a4bc364646d87672b5
thonbecker <thon.becker@gmail.com>
2025-10-04 14:10:33 -0500

    feat: update Java version to 25 and implement integration testing with PostgreSQL

---
+++ src/test/java/com/thonbecker/foosball/AbstractIntegrationTest.java
@@ -0,0 +1,31 @@
+package com.thonbecker.foosball;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void overrideProps(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgres::getDriverClassName);
+        registry.add("spring.liquibase.user", postgres::getUsername);
+        registry.add("spring.liquibase.password", postgres::getPassword);
+    }
+}
--- src/test/java/com/thonbecker/foosball/FoosballApplicationTests.java
+++ src/test/java/com/thonbecker/foosball/FoosballApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FoosballApplicationTests {
+class FoosballApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {}
